771aaf3e4c3b28e3d349d3b5551c0e6f14a8d308
Sagnik Mazumder <39977582+Karnak123@users.noreply.github.com>
2019-12-31 14:53:29 +0530

    Added code as per requirement

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-    int ch,sum;
+    int ch,result;
     float num1, num2;
     cout << "Enter option \n 1.Addition \t 2. Subtraction \n 3.Multiplication \t 4. Division \n 5. Modulus \t 6. Maximum of two nos \n ";
     cin >> ch;
@@ -13,8 +13,28 @@ int main()
     switch(ch)
     {
         case 1:
-            sum=num1+num2;
-            cout<<"Sum of two numbers is "<<sum;
+            result=num1+num2;
+            cout<<"Sum of two numbers is "<<result;
+            break;
+        case 2:
+            result=num1-num2;
+            cout<<"Difference of two numbers is "<<result;
+            break;
+        case 3:
+            result=num1*num2;
+            cout<<"Multiplication of two numbers is "<<result;
+            break;
+        case 4:
+            result=num1/num2;
+            cout<<"Division of two numbers is "<<result;
+            break;
+        case 5:
+            result=num1%num2;
+            cout<<"Modulus of two numbers is "<<result;
+            break;
+        case 6:
+            result=(num1>num2)?num1:num2;
+            cout<<result<<" is greater if two numbers";
             break;
         default:
             // If the options is not 1-6, error message is shown
