cf7520bbcc237a429f620e311a49ee315520c6b4
sush_kd <54639137+sushant-kd2019@users.noreply.github.com>
2019-12-31 17:14:37 +0530

    Completed the program.

--- CheckCharacter.py
+++ CheckCharacter.py
@@ -1,3 +1,21 @@
 # A program to check if the a character is a Capital Letter, Small Letter,vowel,consonant, Digit or special Character .
 
 ch = input("Enter a character: ") #character input
+ascii=ord(ch)
+if 65<=ascii<=90:
+	print("Capital Lettar")
+	if ch in ['A','E','I','O','U']:
+		print("Vowel")
+	else:
+		print("Consonant")
+		
+elif 97<=ascii<=122:
+	print("Small Letter")
+	if ch in ['a','e','i','o','u']:
+		print("Vowel")
+	else:
+		print("Consonant")
+elif 48<=ascii<=57:
+	print("Digit")
+else:
+	print("Special Character")
