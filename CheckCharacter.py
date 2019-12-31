bc4eb9ce0783fa8b8efc17a410911c4f10f13aae
Ankit Raj <ankit.raj928@gmail.com>
2019-12-31 21:26:32 +0530

    Update CheckCharacter.py
    Completed

--- CheckCharacter.py
+++ CheckCharacter.py
@@ -1,3 +1,9 @@
 # A program to check if the a character is a Capital Letter, Small Letter,vowel,consonant, Digit or special Character .
 
 ch = input("Enter a character: ") #character input
+if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')): 
+    print("The Given Character ", ch, "is an Alphabet") 
+elif(ch >= '0' and ch <= '9'):
+    print("The Given Character ", ch, "is a Digit")
+else:
+    print("The Given Character ", ch, "is a Special Character")
