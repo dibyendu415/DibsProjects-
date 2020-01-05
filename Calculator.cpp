680672c4166736738cc53c5c501f71a4fef081a6
Ankit Raj <ankit.raj928@gmail.com>
2020-01-05 22:13:31 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,10 +1,19 @@
 //Basic Calculator using C++
 
 # include <iostream>
+#include <cmath>
 using namespace std;
+
+template <typename T, typename U>
+T find_max(T a, U b)
+{
+    return a > b ? a : b;
+}
 int main()
 {
-    int ch,sum;
+    int ch;
+    float sum, sub, mul, div,max;
+    int mod;
     float num1, num2;
     cout << "Enter option \n 1.Addition \t 2. Subtraction \n 3.Multiplication \t 4. Division \n 5. Modulus \t 6. Maximum of two nos \n ";
     cin >> ch;
@@ -13,9 +22,35 @@ int main()
     switch(ch)
     {
         case 1:
-            sum=num1+num2;
-            cout<<"Sum of two numbers is "<<sum;
+            sum = num1 + num2;
+            cout << "Sum of two numbers is " << sum;
             break;
+
+        case 2:
+        	sub = (num1 - num2);
+        	cout << "Difference of two numbers is " << sub;
+        	break;
+
+        case 3:
+        	mul = num1 * num2;
+        	cout << "Product of two numbers is " << mul;
+        	break;
+
+        case 4:
+        	div = num1 / num2;
+        	cout << "Division of two numbers is " << div;
+        	break;
+
+        case 5:
+        	mod = fmod(num1, num2);
+        	cout << "Modulus of two number is " << mod;
+        	break;
+
+        case 6:
+        	max = find_max(num1, num2);
+        	cout << "Maximum number is " << max;
+        	break;
+
         default:
             // If the options is not 1-6, error message is shown
             cout << "Error! Option is not correct";
