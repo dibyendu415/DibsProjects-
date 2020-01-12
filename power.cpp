7dddc90db2845bdfc8730744f3f5b5b30c7a5b2a
Ankit Raj <ankit.raj928@gmail.com>
2020-01-12 23:34:53 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,39 @@
+//
+// Created by ankit on 05-08-2019.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef int ii;
+typedef unsigned  u;
+int power(ii x, u y)
+{
+    ii result = 1;
+
+    while (y > 0)
+    {
+        if (y & 1)
+            result = result * x;
+
+        y = y >> 1;
+        x = x * x;
+    }
+    return result;
+}
+int main(void)
+{
+    ii x;
+    u y;
+    cin >> x >> y;
+    cout << power(x, y) << endl;
+}
+
+
+
+
+
+
+
+
+
